5cdd349d1b42a0f4771df445d99d783daf0b91d9
Bodanor <papadopoulos.chris@icloud.com>
2021-01-30 20:11:20 +0100

    Ajout de couleurs dans le temrinal + Choix d'un serveur connecte des le debut et plus de random
    La commande serveur est plus smart qu'avant

--- New/passGen.py
+++ New/passGen.py
@@ -8,8 +8,20 @@ import os
 import fcntl
 import sys
 
-IP = ["94.106.244.227"]
+IP = ["94.106.244.227", "127.0.0.1"]
 PORT = 7654
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 def passwordEncoder(password_holder):
 
@@ -29,7 +41,7 @@ def passwordEncoder(password_holder):
 
 
 def commandInput():
-    command = input("passGen >> ")
+    command = input(bcolors.OKGREEN+ "passGen >> " + bcolors.ENDC)
     return command
 
 
@@ -69,7 +81,7 @@ def commandWorker():
 
                     validchoice = True
                     for index, decoded_password in enumerate(password_holder):
-                        print("MDP {} : ".format(index + 1), decoded_password)
+                        print("MDP {} : ".format(index + 1), bcolors.OKBLUE + decoded_password + bcolors.ENDC)
 
 
                 elif choice == "N" or choice == "n":
@@ -94,7 +106,7 @@ def commandWorker():
                     with open('Passwords', 'wb') as file:
                         pickle.dump(encoded_password_holder, file)
 
-                    print("Le fichier \"Passwords\" contient vos mot de passe")
+                    print("Le fichier " + bcolors.WARNING + "\"Passwords\"" + bcolors.ENDC + " contient vos mot de passe")
                     validchoice = True
 
                 elif save == "N" or save == "n":
@@ -109,14 +121,72 @@ def commandWorker():
             if len(args) == 1:
                 crash = connexionStatus(server_status)
 
-            if crash == True:
-                print("Disconnected !")
+                if crash == True:
+                    print(bcolors.FAIL + "Disconnected !" + bcolors.ENDC)
 
-            elif crash == False:
-                print("Connected !")
+                elif crash == False:
+                    print(bcolors.OKGREEN + "Connected !" + bcolors.ENDC)
+
+
+        if args[0] == "Serveur" or args[0] == "SERVEUR" or args[0] == "serveur" or args[0] == "serv":
+            serveur_up = []
+            if len(args) == 1:
+                for serveur in IP:
+                    try:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        s.connect((serveur, PORT))
+                        s.close()
+                        serveur_up.append(serveur)
+
+                    except:
+                        pass
+
+                for index, serveur in enumerate(serveur_up):
+                    print(bcolors.OKGREEN + "Serveur {} [{}] : est fonctionnel !".format(index+1, serveur) + bcolors.ENDC)
+
+
+                #faire une boucle et afficher les serveur disponibles
+
+            elif args[1] == "-a":
+                serveur_up = []
+                serveur_down = []
+
+                for serveur in IP:
+                    try:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        s.connect((serveur, PORT))
+                        s.close()
+                        serveur_up.append(serveur)
+
+                    except:
+                        serveur_down.append(serveur)
+                        pass
+
+                for index, serveur in enumerate(serveur_up):
+                    print(bcolors.OKGREEN + "Serveur {} [{}] : est fonctionnel !".format(index+1, serveur) + bcolors.ENDC)
+
+                for index, serveur in enumerate(serveur_down):
+                    print(bcolors.FAIL + "Serveur {} [{}] : est down !".format(index + 1, serveur) + bcolors.ENDC)
+
+
+
+                #Faire une boucle et afficher serveurs disponible et non disponibles
+
+
 
         if args[0] == "exit" or args[0] == "Exit" or args[0] == "ex":
             os._exit(0)
+
+        if args[0] == "Help" or args[0] == "help" or args[0] == "HELP":
+            print(bcolors.WARNING)
+            print("Liste de commande disponible : ")
+            print("\t Gen : Génerer des mot de passe. Utilisation : Gen <Nombre de mot de passe> <Longueur du mot de passe>")
+            print("\t Status : Afficher le status de connexion")
+            print("\t Server : Afficher les serveurs disponibles. Utilisation : -a pour afficher tout les serveurs")
+            print("\t Exit : Quitter le programme.")
+            print("\t Help : afficher ce menu.")
+            print("Plus de fonctionne viendront quand les idées seront la.")
+            print(bcolors.ENDC)
 
 
 
@@ -150,10 +220,21 @@ def connexionHolder():
 
             server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server_status = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            serveur_up = []
+            try:
+                for serveur in IP:
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    s.connect((serveur, PORT))
+                    s.close()
+                    serveur_up.append(serveur)
 
-            random_IP = IP[random.randint(0,len(IP)-1)]
+            except:
+                pass
+
+            random_IP = serveur_up[random.randint(0,len(serveur_up)-1)]
             server.connect((random_IP, PORT))
             server_status.connect((random_IP, PORT))
+            print(bcolors.WARNING + "Connecté au serveur {}".format(random_IP) + bcolors.ENDC)
             fcntl.fcntl(server_status, fcntl.F_SETFL, os.O_NONBLOCK)
             connected = True
             command_threader = threading.Thread(target=commandWorker)
